a64931021c61d97e3b8052e3501f63a6265690a3
OK <gildean@salaliitto.com>
2012-10-08 23:32:29 +0300

    Fix zero

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var http = require('http'),
 http.createServer(function (req, res) {
 	res.writeHead(200, {'Content-Type': 'text/html'});
 	res.end(nodeStr + '<h3>InfoNode uses: ' + (process.memoryUsage()['rss']/1000000).toFixed(2) + ' MB of memory</h3>\n<h3>Server has: ' + (os.freemem()/1000000).toFixed(0) + ' / ' + (os.totalmem()/1000000).toFixed(0) + ' MB of memory free</h3>\n'
-					+ '<h4>InfoNode uptime: ' + (process.uptime()/360).toFixed(2) + ' hours</h4>\n<h4>Server uptime: ' + (os.uptime()/360).toFixed(2) + ' hours</h4>'
+					+ '<h4>InfoNode uptime: ' + (process.uptime().toFixed(0)/3600).toFixed(2) + ' hours</h4>\n<h4>Server uptime: ' + (os.uptime()/360).toFixed(2) + ' hours</h4>'
 					+ '<h3>Server load:</h3>\n<p><strong>1min:</strong> ' + os.loadavg()[0].toFixed(4) + ', <strong>5min:</strong> ' + os.loadavg()[1].toFixed(4) + ', <strong>15min:</strong> ' + os.loadavg()[2].toFixed(4) + '</p>'
 					+ '</div></body></html>\n');
 }).listen(1337);
